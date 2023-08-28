42bfcebe376465b83a232e52f4e328a16ce50663
AdolfoHMtz <102195837+AdolfoHMtz@users.noreply.github.com>
2023-08-28 12:45:54 -0600

    Programa1.cpp
    Commit estetico

--- Programa1.cpp
+++ Programa1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 //Adolfo Huerta Martínez//
+//Programa 1 Programacion I //
 //Declaracion de las variables//
 void menu();
 float areaCir();
@@ -42,13 +43,13 @@ int main(){
 }
 //Menu de opciones disponibles//
 void menu() {
-    cout<<"Ingresa el numero del calculo que quieres realizar"<<endl;
-    cout<<"1) Calcular el area del Circulo"<<endl;
-    cout<<"2) Calcular el area del Rectangulo"<<endl;
-    cout<<"3) Calcular el area del Triangulo Rectangulo"<<endl;
-    cout<<"4) Calcular el perimetro del Rectangulo"<<endl;
-    cout<<"5) Calcular el perimetro del Romboide"<<endl;
-    cout<<"6) Calcular el perimetro del Triangulo Isoceles"<<endl;
+    cout<<"-----Ingresa el numero del calculo que quieres realizar-----"<<endl;
+    cout<<"\t1) Calcular el area del Circulo"<<endl;
+    cout<<"\t2) Calcular el area del Rectangulo"<<endl;
+    cout<<"\t3) Calcular el area del Triangulo Rectangulo"<<endl;
+    cout<<"\t4) Calcular el perimetro del Cuadrado"<<endl;
+    cout<<"\t5) Calcular el perimetro del Romboide"<<endl;
+    cout<<"\t6) Calcular el perimetro del Triangulo Isoceles"<<endl;
 }
 //Calculo de area del circulo//
 float areaCir() {
@@ -62,55 +63,55 @@ float areaCir() {
 //Calculo de area del rectangulo//
 float areaRect() {
     float ladoA, ladoB;
-    cout<<"Ingresa la medida del lado A: ";
+    cout<<"\tIngresa la medida del lado A: ";
     cin>>ladoA;
-    cout<<"Ingresa la medida del lado B: ";
+    cout<<"\tIngresa la medida del lado B: ";
     cin>>ladoB;
     float area=ladoA*ladoB;
-    cout<<"El area del Rectangulo es de: "<<area<<endl;
+    cout<<"\tEl area del Rectangulo es de: "<<area<<endl;
     return area;
 }
 //Calculo de area del Triangulo Rectangulo//
 float areaTriRect() {
     float base, alt;
-    cout<<"Ingresa la base del triangulo: ";
+    cout<<"\tIngresa la base del Triangulo Rectangulo: ";
     cin>>base;
-    cout<<"Ingresa la altura del triangulo: ";
+    cout<<"\tIngresa la altura del Triangulo Rectangulo: ";
     cin>>alt;
     float area=(base*alt)/2;
-    cout<<"El area del Triangulo Rectangulo es de: " <<area<<endl;
+    cout<<"\tEl area del Triangulo Rectangulo es de: " <<area<<endl;
     return area;
 }
 //Calculo de perimetro del Cuadrado//
 float perimCuad() {
     float lado;
-    cout<<"Ingresa la medida del lado del cuadrado: ";
+    cout<<"\tIngresa la medida del lado del cuadrado: ";
     cin>>lado;
     float perimetro = lado*4;
-    cout<<"El perimetro del Cuadrado es de: "<<perimetro<<endl;
+    cout<<"\tEl perimetro del Cuadrado es de: "<<perimetro<<endl;
     return perimetro;
  }
 //Calculo de perimetro del Romboide//
 float perimRomb(){
     float ladoA,ladoB;
-    cout<<"Ingresa la medida del lado A: ";
+    cout<<"\tIngresa la medida del lado A: ";
     cin>>ladoA;
-    cout<<"Ingresa la medida del lado B: ";
+    cout<<"\tIngresa la medida del lado B: ";
     cin>>ladoB;
     float perimetro=(ladoA*2)+(ladoB*2); 
-    cout<<"El perimetro del Romboide es de: "<<perimetro<<endl;
+    cout<<"\tEl perimetro del Romboide es de: "<<perimetro<<endl;
     return perimetro;
 }
 //Calculo de perimetro del Triangulo Isoceles//
 float perimIsoce(){
     float ladoA,ladoB,ladoC;
-    cout<<"Ingresa la medida del lado A: ";
+    cout<<"\tIngresa la medida del lado A: ";
     cin>>ladoA;
-    cout<<"Ingresa la medida del lado B: ";
+    cout<<"\tIngresa la medida del lado B: ";
     cin>>ladoB;
-    cout<<"Ingresa la medida del lado C: ";
+    cout<<"\tIngresa la medida del lado C: ";
     cin>>ladoC;
     float perimetro = ladoA+ladoB+ladoC; 
-    cout<<"El perimetro del Triangulo Isoceles es de: "<<perimetro<<endl;
+    cout<<"\tEl perimetro del Triangulo Isoceles es de: "<<perimetro<<endl;
     return perimetro;
 }
